7277e3b081e7437fdf9e9451c54a125d80b2909a
michael <michael.nucci@gmail.com>
2015-06-13 12:43:06 -0700

    changed to google-like style

--- output.hpp
+++ output.hpp
@@ -14,27 +14,30 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
-#ifndef OUTPUTHEADERDEF             //only if the macro OUTPUTHEADERDEF is not defined execute these lines of code
-#define OUTPUTHEADERDEF             //define the macro
+#ifndef OUTPUTHEADERDEF  // only if the macro OUTPUTHEADERDEF is not defined
+                         // execute these lines of code
+#define OUTPUTHEADERDEF  // define the macro
 
 /* This header contains the function declarations for file output.
 
-It contains function headers to write out the grid at the cell centers in Plot3D format, as well as the Plot3D function files.
-It also writes out a master file in Ensight format to name the Plot3D functions.  */
+It contains function headers to write out the grid at the cell centers in Plot3D
+format, as well as the Plot3D function files.
+It also writes out a master file in Ensight format to name the Plot3D functions.
+*/
 
-#include <vector>  //vector
-#include <string>  //string
-#include "vector3d.hpp" //vector3d
-#include "tensor.hpp" //tensor
-#include "plot3d.hpp" //plot3d
-#include "eos.hpp"
-#include "primVars.hpp" //primVars
-#include "procBlock.hpp" //procBlock
-#include "inviscidFlux.hpp" //inviscidFlux
-#include "input.hpp" //inputVars
-#include "boundaryConditions.hpp" //decomposition
 #include <fstream>
 #include <iostream>
+#include <vector>        // vector
+#include <string>        // string
+#include "vector3d.hpp"  // vector3d
+#include "tensor.hpp"    // tensor
+#include "plot3d.hpp"    // plot3d
+#include "eos.hpp"
+#include "primVars.hpp"            // primVars
+#include "procBlock.hpp"           // procBlock
+#include "inviscidFlux.hpp"        // inviscidFlux
+#include "input.hpp"               // inputVars
+#include "boundaryConditions.hpp"  // decomposition
 
 using std::vector;
 using std::string;
@@ -44,15 +47,20 @@ using std::cout;
 using std::endl;
 using std::cerr;
 
-//function definitions
-void WriteCellCenter(const string&, const vector<procBlock> &, const decomposition&, const double&);
-void WriteCellCenterGhost(const string&, const vector<procBlock> &);
-void WriteFun(const string&, const vector<procBlock> &, const idealGas&, const sutherland&, const int&, const decomposition&, const input&);
-void WriteRes(const string&, const int&, const int&);
+// function definitions
+void WriteCellCenter(const string &, const vector<procBlock> &,
+                     const decomposition &, const double &);
+void WriteCellCenterGhost(const string &, const vector<procBlock> &);
+void WriteFun(const string &, const vector<procBlock> &, const idealGas &,
+              const sutherland &, const int &, const decomposition &,
+              const input &);
+void WriteRes(const string &, const int &, const int &);
 
-void WriteResiduals(const input&, genArray&, genArray&, const resid&, const double&, const int&, const int&);
+void WriteResiduals(const input &, genArray &, genArray &, const resid &,
+                    const double &, const int &, const int &);
 
-vector<procBlock> Recombine( const vector<procBlock>&, const decomposition& );
-int SplitBlockNumber( const vector<procBlock>&, const decomposition&, const int&, const int&, const int&, const int& );
+vector<procBlock> Recombine(const vector<procBlock> &, const decomposition &);
+int SplitBlockNumber(const vector<procBlock> &, const decomposition &,
+                     const int &, const int &, const int &, const int &);
 
 #endif
