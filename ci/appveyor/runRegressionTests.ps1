c879e5a59562dd48323a79a58ba86e701c7fdca4
Michael <michael.nucci@gmail.com>
2017-03-19 18:14:13 -0700

    Appveyor CI changes.

--- ci/appveyor/runRegressionTests.ps1
+++ ci/appveyor/runRegressionTests.ps1
@@ -1,3 +1,11 @@
-# Run regression tests
-cd testCases
-python3 regressionTests.py --mpirunPath="C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" --aitherPath="C:\Program Files\Project\bin\aither.exe"
+function RunTests() {
+    # Run regression tests
+    cd testCases
+    python3 regressionTests.py --mpirunPath="C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" --aitherPath="C:\Program Files\Project\bin\aither.exe"
+}
+
+function main() {
+    RunTests()
+}
+
+main
