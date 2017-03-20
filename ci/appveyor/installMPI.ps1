c0cbe4d25871a29881a91f454b6f6c4be5d7933a
Michael <michael.nucci@gmail.com>
2017-03-19 19:55:00 -0700

    Appveyor fixes.

--- ci/appveyor/installMPI.ps1
+++ ci/appveyor/installMPI.ps1
@@ -9,18 +9,6 @@ function InstallMPI() {
     Write-Host "Installing Microsoft MPI Runtime..."
     appveyor DownloadFile http://download.microsoft.com/download/B/2/E/B2EB83FE-98C2-4156-834A-E1711E6884FB/MSMpiSetup.exe
     Start-Process -FilePath MSMpiSetup.exe -ArgumentList -unattend -Wait
-    # Get MPI environment variables
-    $envfile = "MPI_Env.cmd"
-    Write-Host "Saving Microsoft MPI environment variables to" $envfile
-    $envlist = @("MSMPI_BIN", "MSMPI_INC", "MSMPI_LIB32", "MSMPI_LIB64")
-    $stream = [IO.StreamWriter] $envfile
-    foreach ($variable in $envlist) {
-        $value = [Environment]::GetEnvironmentVariable($variable, "Machine")
-        if ($value) { $stream.WriteLine("SET $variable=$value") }
-        if ($value) { Write-Host "$variable=$value" }
-    }
-    $stream.Close()
-    & $envfile
     cd ..
 }
 
