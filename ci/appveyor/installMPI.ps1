7aee16f51768ad5fa98d65f1f36bde362b427c43
Michael <michael.nucci@gmail.com>
2018-02-04 21:33:18 -0800

    trying appveyor cmake fix

--- ci/appveyor/installMPI.ps1
+++ ci/appveyor/installMPI.ps1
@@ -14,14 +14,22 @@ function InstallMPI() {
 }
 
 function InstallCmake() {
-    md cmake-38
-    cd cmake-38
+    Write-Host "Installing CMake 3.4.0 ..." -ForegroundColor Cyan
+    $exePath = "$($env:USERPROFILE)\cmake-3.4.0-rc2-win32-x86.exe"
+    Write-Host "Downloading..."
+    (New-Object Net.WebClient).DownloadFile('https://cmake.org/files/v3.4/cmake-3.4.0-rc2-win32-x86.exe', $exePath)
+    Write-Host "Installing..."
+    cmd /c start /wait $exePath /S
+    cmake --version
+    Write-Host "CMake 3.4.0 installed" -ForegroundColor Green
+    #md cmake-38
+    #cd cmake-38
     # install cmake
-    Write-Host "Installing CMake 3.8.0 ..."
-    appveyor DownloadFile https://cmake.org/files/v3.8/cmake-3.8.0-win64-x64.msi
-    Start-Process -FilePath msiexec.exe -ArgumentList "/quiet /qn /i cmake-3.8.0-win64-x64.msi" -Wait
-    Write-Host "CMake 3.8.0 installed..."
-    cd ..
+    #Write-Host "Installing CMake 3.8.0 ..."
+    #appveyor DownloadFile https://cmake.org/files/v3.8/cmake-3.8.0-win64-x64.msi
+    #Start-Process -FilePath msiexec.exe -ArgumentList "/quiet /qn /i cmake-3.8.0-win64-x64.msi" -Wait
+    #Write-Host "CMake 3.8.0 installed..."
+    #cd ..
 }
 
 function main() {
