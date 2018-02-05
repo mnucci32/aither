3b06feebfa4b7bec3d8b2e7e744be9a0e48c3cf5
Michael <michael.nucci@gmail.com>
2018-02-05 07:40:36 -0800

    Updated license year

--- ci/appveyor/installMPI.ps1
+++ ci/appveyor/installMPI.ps1
@@ -22,14 +22,6 @@ function InstallCmake() {
     cmd /c start /wait $exePath /S
     cmake --version
     Write-Host "CMake 3.4.0 installed" -ForegroundColor Green
-    #md cmake-38
-    #cd cmake-38
-    # install cmake
-    #Write-Host "Installing CMake 3.8.0 ..."
-    #appveyor DownloadFile https://cmake.org/files/v3.8/cmake-3.8.0-win64-x64.msi
-    #Start-Process -FilePath msiexec.exe -ArgumentList "/quiet /qn /i cmake-3.8.0-win64-x64.msi" -Wait
-    #Write-Host "CMake 3.8.0 installed..."
-    #cd ..
 }
 
 function main() {
