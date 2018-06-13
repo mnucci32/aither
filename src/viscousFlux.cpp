466b7a9edd2d105d18dd746ad424107e4cd58b68
Michael <mnucci@pm.me>
2018-06-13 16:59:17 -0700

    Updating contact email [ci skip]

--- src/viscousFlux.cpp
+++ src/viscousFlux.cpp
@@ -1,5 +1,5 @@
 /*  This file is part of aither.
-    Copyright (C) 2015-18  Michael Nucci (michael.nucci@gmail.com)
+    Copyright (C) 2015-18  Michael Nucci (mnucci@pm.me)
 
     Aither is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
